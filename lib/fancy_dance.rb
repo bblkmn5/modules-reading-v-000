260683123d2ca4d9a35ed90bdae52d0fac4c2691
Ben Blackman <bblkmn5@gmail.com>
2017-10-25 23:41:40 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,6 +17,9 @@ module FancyDance
     end
   end
 
-  module ClassMethods 
-  
+  module ClassMethods
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
 end
